e1182cb00802072eef57f749c441a69c20ba677a
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-30 21:59:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,8 +5,8 @@ class Anagram
   def initialize(word)
     @word = word
   end
-  
- array_of_words.select do |element|
+def match(array_of_words)
+  array_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
     end
   end 
