014bed67f699ecb67f717f91ba8d55923c67c4e0
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-30 21:57:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,8 +6,9 @@ class Anagram
     @word = word
   end
   
-def match(word_array)
+ def match(word_array)
     word_array.select do |word|
       word.split("").sort == @word.split("").sort
     end
-  end
+  end 
+  
