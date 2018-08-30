cea138a2fd3e87daf015634a0fe2e8971b31ebef
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-30 22:01:13 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,9 +2,10 @@ class Anagram
   
   attr_accessor :word
   
-  def initialize(word)
+def initialize(word)
     @word = word
   end
+  
 def match(word_array)
   word_array.select do |word|
       word.split("").sort == word.split("").sort
